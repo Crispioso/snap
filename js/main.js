9fa4f1394e8c565871aad3c08aaf00aafba379f9
Crispioso <crispin.merriman@gmail.com>
2015-10-17 21:54:22 +0100

    Numerous updates

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 $( document ).ready(function(){
 	//List functions you're using here. The order matters..
 	addActive();
+	animateScroll();
 
 
 
@@ -20,20 +21,14 @@ $( document ).ready(function(){
 		//Loop to get id attribute of each heading 2
 		$('h2').each(function(index){
 			heading[index] = $(this).attr('id');
-			// console.log('heading = #' + heading[index]);
 
-			headingPos[index] = $('#' + heading[index]).offset().top - 72;
-			// console.log('position = ' + headingPos[index]);
+			headingPos[index] = $('#' + heading[index]).offset().top - 76;
 		});
 
+		//Current scroll position on page
 		var winScroll = $(document).scrollTop();
-		var winH      = $(window).height();
 
-		// console.log('Scroll position = ' + winScroll);
-		// console.log('Heading 1 position = ' + headingPos[0]);
-
-
-		//Stores the generated if statements
+		//Stores the generated if statements - saves writing them out manually.
 		var ifLoop = [];
 
 		//Function to check if active link needs to be added
@@ -42,16 +37,21 @@ $( document ).ready(function(){
 				//Store heading name for re-use
 				var navLink = '#' + heading[index];
 
-				if( winScroll >= headingPos[index] && winScroll < headingPos[index+1] ) {
+				//If scroll position equal or more than heading position add class. If last in array and scroll position more then add class. Else remove class.
+				if (winScroll == 0) {
+					$('a[href="#home"]').css('color', '#551A8B');
 
+				} else if( winScroll >= headingPos[index] && winScroll < headingPos[index+1] ){
 					$('a[href="' + navLink + '"]').addClass('nav__link--active');
+					$('a[href="#home"]').css('color', '#323132');
 
 				} else if( winScroll >= headingPos[index] && index == heading.length-1 ) {
-
 					$('a[href="' + navLink + '"]').addClass('nav__link--active');
+					$('a[href="#home"]').css('color', '#323132');
 
 				} else {
 					$('a[href="' + navLink + '"]').removeClass('nav__link--active');
+					$('a[href="#home"]').css('color', '#323132');
 				}
 			}
 		}
@@ -75,6 +75,58 @@ $( document ).ready(function(){
 		});
 	}
 
+
+	//Animate scroll to any hashes from anchor click
+	function animateScroll() {
+		//On page load
+		$(document).ready(function(e) {
+			//e.preventDefault();
+
+			if(location.hash) {
+				//Get hash part of href from anchor
+				var target = location.hash;
+
+				//Animated scroll to location
+				if (target == '#home' ) {
+					//Go to absolute top of page if clicking 'home'
+					$('html, body').animate({scrollTop: $(target).offset().top - 124}, 1000, function(){
+						location.hash = target;
+					});
+				} else {
+					$('html, body').animate({scrollTop: $(target).offset().top - 70}, 1000, function(){
+						location.hash = target;
+					});
+				};
+			}
+		});
+
+
+		//On click
+		$('a').click(function(e) {
+			e.preventDefault();
+
+			//Get hash part of href from anchor
+			var target = this.hash;
+			
+			//Add class to show it's active
+			// $(this).addClass('nav__link--active');
+
+			//Animated scroll to location
+			if (target == '#home' ) {
+				//Go to absolute top of page if clicking 'home'
+				$('html, body').animate({scrollTop: $(target).offset().top - 124}, 1000, function(){
+					location.hash = target;
+				});
+			} else {
+				$('html, body').animate({scrollTop: $(target).offset().top - 70}, 1000, function(){
+					location.hash = target;
+				});
+			};
+
+			
+			
+		})
+	}
 
 	//END OF FUNCTIONS
 });
